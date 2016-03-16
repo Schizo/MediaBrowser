845b754b9a7712571e2703f8222db2f0853e00eb
Schizo <yafes7@yahoo.de>
2016-03-16 09:26:20 -0700

    Revert "GUI for sequence adder"

--- python/browserViewer.py
+++ python/browserViewer.py
@@ -14,79 +14,38 @@ from PyQt4.QtGui import *
 from browserCategories import BrowserCategories
 from mimeGenerator import MimeGenerator
 
-import subprocess
-
 class ThumbContextMenu(QtGui.QMenu):
     def __init__(self):
         super(ThumbContextMenu, self).__init__()
-
-        # disabled, since it has been broken in the old ElementsBrowser for a few months and no one complained
-        # self.addAction("Open EXR Photoshop", self.openPhotoshopEXR)
-        # self.addAction("Open JPG Photoshop", self.openPhotoshopJPG)
-
-        self.addAction("Open EXR in RV", self.openRVEXR)
-        self.addAction("Open JPG in RV", self.openRVJPG)
-        # self.addAction("Open EXR in djv", self.openDJVEXR)
-        # self.addAction("Open JPG in djv", self.openDJVJPG)
+        self.addAction("Open EXR Photoshop", self.openPhotoshop)
+        self.addAction("Open JPG Photoshop", self.openPhotoshop)
+        self.addAction("Open EXR in RV", self.openRV)
+        self.addAction("Open JPG in RV", self.openRV)
         self.addAction("Open Folder Location", self.openFolder)
-        self.addAction("Remove from DB", self.removeFromDB)
+        self.addAction("Remove from DB")
 
     def setFileData(self, scrubFrame, fileName):
-        # note: using scrubFrame.zfill is logically incorrect, since the frame numbers
-        #       on thhumbnails go to 50 while frame numbers on real footage go from
-        #       startFrame to startFrame + numOfFrames
-        #       to get the source file number corresponding ot a thumb:
-        #       startFrame + (scrubFrame / float(50)) * numOfFrames
+        constructPath = settings.sourcePath(settings.currentCategory, fileName)
+        self.openPath = constructPath.replace('####', scrubFrame.zfill(4))
 
-        # right now, we just open the folder instead, which loads all frames
 
-        # constructPath = settings.sourcePath(settings.currentCategory, fileName)
-        # self.openPathEXR = constructPath.replace('####', scrubFrame.zfill(4))
-        self.fileName = fileName
+    def openPhotoshop(self):
+        print "opening Photoshop"
+        print self.openPath
 
-        self.openPathEXR = os.path.split(settings.sourcePath(settings.currentCategory, fileName))[0]
-        self.openPathJPG = os.path.split(settings.proxyPath(settings.currentCategory, fileName))[0]
-
-        self.locationPath = settings.locationPath(settings.currentCategory, fileName)
-
-    # def openPhotoshopEXR(self):
-    #     print "opening Photoshop"
-    #     print self.openPathEXR
-    #
-    # def openPhotoshopEXR(self):
-    #     print "opening Photoshop"
-    #     print self.openPathJPG
-
-    def openRVEXR(self):
-        # print "opening RV"
-        # print "rv: " + settings.appPath["rv"]
-        # print self.openPathEXR
-        subprocess.Popen([settings.appPath["rv"], self.openPathEXR])
-
-    def openRVJPG(self):
+    def openRV(self):
         print "opening RV"
-        subprocess.Popen([settings.appPath["rv"], self.openPathJPG])
-
-    # def openDJVEXR(self):
-    #     os.system(settings.appPath["djv"] + " " + self.openPathEXR)
-    #     # subprocess.Popen([settings.appPath["djv"], self.openPathEXR])
-    #
-    # def openDJVJPG(self):
-    #     subprocess.Popen([settings.appPath["djv"], self.openPathJPG + "/*"])
 
     def openFolder(self):
-        subprocess.Popen([settings.appPath["explorer"], self.locationPath.replace("/","\\")])
-
-    def removeFromDB(self):
-        settings.removeItem(settings.currentCategory, self.fileName)
+        print "opening Folder"
 
 
 
 class BrowserViewer(QtGui.QListWidget):
-
+    
     categoryChanged = pyqtSignal(str, name='categoryChanged')
     signalItemDragged = pyqtSignal(str, name='itemDragged')
-
+    
 
     def __init__(self, parent):
         self.parent = parent
@@ -94,21 +53,21 @@ class BrowserViewer(QtGui.QListWidget):
         self.currentCategory = settings.showConfig["startCategory"]
         self.initUI()
         self.menu = ThumbContextMenu()
-
+        
         self.mimeGenerator = MimeGenerator(self)
         self.categoryChanged.connect(self.changeCategory)
         self.signalItemDragged.connect(self.mimeGenerator.signalItemDragged)
 
         self.setContextMenuPolicy(QtCore.Qt.ActionsContextMenu)
-        # self.customContextMenuRequested.connect(self.openMenu)
+        self.customContextMenuRequested.connect(self.openMenu)
 
 
     def changeCategory(self, item):
         self.currentCategory = str(item)
-        settings.currentCategory = self.currentCategory #todo make it less locally dependend,
+        settings.currentCategory = self.currentCategory #todo make it less locally dependend, 
         #as this one is called from the browserViewerItems
         self.populateWidgets(settings.pathCache[self.currentCategory])
-
+   
 
     def initUI(self):
         self.setSelectionMode(QtGui.QAbstractItemView.ExtendedSelection)
@@ -120,11 +79,11 @@ class BrowserViewer(QtGui.QListWidget):
 
         self.setViewMode(QtGui.QListView.IconMode)
         self.setResizeMode(QtGui.QListView.Adjust)
-
+ 
 
         self.itemHolder = []
 
-        #Initialize Empty Thumbnails
+        #Initialize Empty Thumbnails 
         for pos in range(0, settings.thumbnails["numOfThumbnails"]):
             item = QtGui.QListWidgetItem()
             item.setSizeHint(QtCore.QSize(settings.thumbnails["width"], settings.thumbnails["height"]))
@@ -185,4 +144,4 @@ def main():
 
 
 if __name__ == '__main__':
-    main()
+    main()    
